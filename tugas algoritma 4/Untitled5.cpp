0c051d3e7a3722e83813155342a40acc3a09c1c8
ardi020801 <ardikuswanto10@gmail.com>
2025-04-10 15:09:21 +0700

    Add files via upload
    ardi kuswanto

---
+++ tugas algoritma 4/Untitled5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std; main() {
+	string  nama, kelas, nim, umur, hobi;
+	cout<<"masukan nama:"<<endl;
+	cin>>nama;
+	cout<<"masukan kelas"<<endl;
+	cin>>kelas;
+	cout<<"masukan nim:"<<endl;
+	cin>>nim;
+	cout<<"masukan umur:"<<endl;
+	cin>>nim;
+	cout<<"masukan hobi:"<<endl;
+	cin>>nim;
+	cout<<"==Data Diri==:"<<endl;
+	cout<<"nim:"<<nim<<endl;
+	cout<<"umur:"<<umur<<endl;
+	cout<<"hobi:"<<hobi<<endl;
+	cout<<"nama:"<<nama<<endl;
+	cout<<"kelas:"<<kelas<<endl;
+
+}
